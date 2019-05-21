6f161a9f3d184afdfee0dddfd6106079febc76e5
pete rodrigue <edwardpierrerodrigue@gmail.com>
2019-05-21 17:53:12 -0500

    Update exploring recidivism data.py

--- exploring recidivism data.py
+++ exploring recidivism data.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Import packages
 import pandas as pd
 import numpy as np
 
+# Import offenders dataset
 OFNT3CE1 = pd.read_csv(
     "C:\\Users\\edwar.WJM-SONYLAPTOP\\Desktop\\ncdoc_data\\data\\preprocessed\\OFNT3CE1.csv",
     dtype={'OFFENDER_NC_DOC_ID_NUMBER': str,
@@ -13,48 +15,50 @@ OFNT3CE1 = pd.read_csv(
            'PRIOR_P&P_COMMNT/COMPONENT_ID': str,
            'ORIGINAL_SENTENCE_AUDIT_CODE': str})
 
-OFNT3CE1.shape
-pd.options.display.max_columns = 100
+OFNT3CE1.shape  # Number of rows
+pd.options.display.max_columns = 100  # Set the max number of col to display
 
-
-# for index, value in enumerate(OFNT3CE1.columns):
-#     print(index, value)
-
+# Create a variable that indicates felony offenses
 OFNT3CE1['has_felony'] = np.where(
     OFNT3CE1['PRIMARY_FELONY/MISDEMEANOR_CD.'] == 'FELON', 1, 0)
+# OFNT3CE1.loc[OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER']=='0000006',].head(50)
+# Only keep people that have ever had a felony offense
 OFNT3CE1 = OFNT3CE1.groupby(
     'OFFENDER_NC_DOC_ID_NUMBER').filter(lambda x: x['has_felony'].max() == 1)
-OFNT3CE1.shape
-OFNT3CE1.head()
-OFNT3CE1['SENTENCE_EFFECTIVE(BEGIN)_DATE'].head(50)
-OFNT3CE1.dtypes
-OFNT3CE1['year'] = OFNT3CE1[
-    'SENTENCE_EFFECTIVE(BEGIN)_DATE'].str.slice(0, 4).astype('int64')
-OFNT3CE1.head()
-# OFNT3CE1 = OFNT3CE1.loc[OFNT3CE1['year'] > 2000, :]
-OFNT3CE1 = OFNT3CE1.groupby(
-    'OFFENDER_NC_DOC_ID_NUMBER').filter(
-        lambda x: x['year'].max() > 2000)
-OFNT3CE1.shape
-OFNT3CE1.head()
-OFNT3CE1[['OFFENDER_NC_DOC_ID_NUMBER',
-          'year',
-          'PRIMARY_FELONY/MISDEMEANOR_CD.',
-          'SENTENCE_EFFECTIVE(BEGIN)_DATE',
-          'P&P_COMPONENT_STATUS_DATE',
-          'MINIMUM_SENTENCE_LENGTH',
-          'MAXIMUM_SENTENCE_LENGTH']].head(10)
 
-OFNT3CE1.columns
-OFNT3CE1.head(30)
-OFNT3CE1.loc[OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER'] == '0017471',
-                     ['OFFENDER_NC_DOC_ID_NUMBER',
-                      'COMMITMENT_PREFIX',
-                      'year', 'DATE_OFFENSE_COMMITTED_-_BEGIN',
-                      'PRIMARY_FELONY/MISDEMEANOR_CD.',
-                      'SENTENCE_EFFECTIVE(BEGIN)_DATE',
-                      'SENTENCE_COMPONENT_NUMBER',
-                      'PRIOR_RECORD_LEVEL_CODE']].head(20)
+OFNT3CE1.shape  # Notice we have fewer rows now
+# OFNT3CE1.head()
+# OFNT3CE1['SENTENCE_EFFECTIVE(BEGIN)_DATE'].head(50)
+# OFNT3CE1.dtypes
+
+# Create a year variable with the year of the start of their sentence
+# OFNT3CE1['year'] = OFNT3CE1[
+#     'SENTENCE_EFFECTIVE(BEGIN)_DATE'].str.slice(0, 4).astype('int64')
+# OFNT3CE1.head()
+# # OFNT3CE1 = OFNT3CE1.loc[OFNT3CE1['year'] > 2000, :]
+# OFNT3CE1 = OFNT3CE1.groupby(
+#     'OFFENDER_NC_DOC_ID_NUMBER').filter(
+#         lambda x: x['year'].max() > 2000)
+# OFNT3CE1.shape
+# OFNT3CE1.head()
+# OFNT3CE1[['OFFENDER_NC_DOC_ID_NUMBER',
+#           'year',
+#           'PRIMARY_FELONY/MISDEMEANOR_CD.',
+#           'SENTENCE_EFFECTIVE(BEGIN)_DATE',
+#           'P&P_COMPONENT_STATUS_DATE',
+#           'MINIMUM_SENTENCE_LENGTH',
+#           'MAXIMUM_SENTENCE_LENGTH']].head(10)
+
+# OFNT3CE1.columns
+# OFNT3CE1.head(30)
+# OFNT3CE1.loc[OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER'] == '0017471',
+#                      ['OFFENDER_NC_DOC_ID_NUMBER',
+#                       'COMMITMENT_PREFIX',
+#                       'year', 'DATE_OFFENSE_COMMITTED_-_BEGIN',
+#                       'PRIMARY_FELONY/MISDEMEANOR_CD.',
+#                       'SENTENCE_EFFECTIVE(BEGIN)_DATE',
+#                       'SENTENCE_COMPONENT_NUMBER',
+#                       'PRIOR_RECORD_LEVEL_CODE']].head(20)
 
 # dropping features we don't want to use:
 OFNT3CE1 = OFNT3CE1.drop(['NC_GENERAL_STATUTE_NUMBER',
@@ -72,42 +76,38 @@ OFNT3CE1 = OFNT3CE1.drop(['NC_GENERAL_STATUTE_NUMBER',
 #
 INMT4BB1 = pd.read_csv(
     "C:\\Users\\edwar.WJM-SONYLAPTOP\\Desktop\\ncdoc_data\\data\\preprocessed\\INMT4BB1.csv")
-INMT4BB1.columns
-INMT4BB1.head(10)
-INMT4BB1.loc[INMT4BB1['INMATE_DOC_NUMBER'] == 17471, :].head(20)
-INMT4BB1.head(50)
-INMT4BB1.PAROLE_SUPERVISION_BEGIN_DATE.unique()
+# INMT4BB1.columns
+# INMT4BB1.head(10)
+# INMT4BB1.loc[INMT4BB1['INMATE_DOC_NUMBER'] == 670272, :].head(20)
+# INMT4BB1.loc[INMT4BB1['INMATE_DOC_NUMBER'] == 17471, :].head(20)
+# INMT4BB1.head(50)
+# INMT4BB1.PAROLE_SUPERVISION_BEGIN_DATE.unique()
 # dropping features we don't want to use:
 INMT4BB1 = INMT4BB1.drop(['INMATE_COMPUTATION_STATUS_FLAG',
                           'PROJECTED_RELEASE_DATE_(PRD)',
                           'PAROLE_DISCHARGE_DATE',
                           'PAROLE_SUPERVISION_BEGIN_DATE'], axis=1)
 
-INMT4BB1.head(50)
-OFNT3CE1.head()
-INMT4BB1.loc[INMT4BB1['INMATE_DOC_NUMBER'] == 34, :].head()
-
+# INMT4BB1.head(10)
+# OFNT3CE1.sort_values(['OFFENDER_NC_DOC_ID_NUMBER']).head(10)
+# INMT4BB1.loc[INMT4BB1['INMATE_DOC_NUMBER'] == 34, :].head()
 OFNT3CE1.dtypes
 INMT4BB1.dtypes
-OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER'] = OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER'].astype('int64')
+# OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER'] = OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER'].astype('int64')
+INMT4BB1['clean_ACTUAL_SENTENCE_END_DATE'] = pd.to_datetime(INMT4BB1['ACTUAL_SENTENCE_END_DATE'], errors='coerce')
+# INMT4BB1[INMT4BB1['clean_ACTUAL_SENTENCE_END_DATE'].isnull() == True ].head(10)
+# INMT4BB1 = INMT4BB1[ INMT4BB1['clean_ACTUAL_SENTENCE_END_DATE'].isnull() == False ]
+INMT4BB1_subset = INMT4BB1.loc[(INMT4BB1['clean_ACTUAL_SENTENCE_END_DATE'] >= '2000-01-01') | (INMT4BB1['clean_ACTUAL_SENTENCE_END_DATE'].isnull() == True), :]
+INMT4BB1_subset = INMT4BB1_subset.loc[(INMT4BB1_subset['clean_ACTUAL_SENTENCE_END_DATE'] < '2006-01-01') | (INMT4BB1['clean_ACTUAL_SENTENCE_END_DATE'].isnull() == True), :]
 
-INMT4BB1['ACTUAL_SENTENCE_END_DATE'] = pd.to_datetime(INMT4BB1['ACTUAL_SENTENCE_END_DATE'], errors='ignore')
-
-INMT4BB1_subset = INMT4BB1.loc[INMT4BB1['ACTUAL_SENTENCE_END_DATE'] >= '2000-01-01', :]
-INMT4BB1_subset = INMT4BB1_subset.loc[INMT4BB1_subset['ACTUAL_SENTENCE_END_DATE'] < '2006-01-01', :]
-INMT4BB1_subset = INMT4BB1_subset.loc[INMT4BB1_subset['INMATE_SENTENCE_COMPONENT'] == 1, :]
-INMT4BB1_subset.sort_values('INMATE_DOC_NUMBER').head(30)
 INMT4BB1_subset.shape
 
+OFNT3CE1.applymap(np.isreal)
+OFNT3CE1['OFFENDER_NC_DOC_ID_NUMBER'].astype('int')
+OFNT3CE1.dtypes
+INMT4BB1_subset.dtypes
 
-OFNT3CE1_subset = OFNT3CE1.loc[OFNT3CE1['SENTENCE_COMPONENT_NUMBER'] == 1, :]
-OFNT3CE1_subset.shape
 
-OFNT3CE1_subset.loc[OFNT3CE1_subset['OFFENDER_NC_DOC_ID_NUMBER'] == 114, :]
-
-
-OFNT3CE1_subset['OFFENDER_NC_DOC_ID_NUMBER'].dtypes
-INMT4BB1_subset['INMATE_DOC_NUMBER'].dtypes
 merged = OFNT3CE1_subset.merge(INMT4BB1_subset,
                        left_on=['OFFENDER_NC_DOC_ID_NUMBER', 'COMMITMENT_PREFIX'],
                        right_on=['INMATE_DOC_NUMBER', 'INMATE_COMMITMENT_PREFIX'],
